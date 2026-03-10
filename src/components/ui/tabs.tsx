fe945838f1d0ab18ca64811351105a64995f4617
Zsolt van den Már <zsolt@vandenmar.com>
2026-03-10 14:37:41 -0700

    Center tabs on small screens

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -16,7 +16,7 @@ function Tabs({
       data-slot="tabs"
       data-orientation={orientation}
       className={cn(
-        "group/tabs flex gap-2 data-horizontal:flex-col",
+        "group/tabs flex gap-2 data-horizontal:flex-col items-center justify-center",
         className
       )}
       {...props}
